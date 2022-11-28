5b7a61b451f885b7bfa9e307f76d3e63e02e403e
Ion Cosulschi <cosulschiion20@stud.ase.ro>
2022-11-28 14:03:31 +0200

    Added more atributes to the classes

--- TicketApp/TicketApp/Source.cpp
+++ TicketApp/TicketApp/Source.cpp
@@ -9,13 +9,15 @@ class Event
 	string location;
 	char* eventName;
 	int nr_of_tickets_available; 
-	float* ticket_price;
+	int* nr_of_seats;
 	EventType type;
-	bool IsOnline;
+	bool hasSponsor;
+	static int MIN_NR_TICKETS_BOUGHT; // the minimum number of tickets to be bought in order the event to go on
 
 };
 class Location
 {
+	const int locationID;
 	string city; // The city the event is located
 	int* nr_seats_available; // depend on the number of rows the location has
 	int nr_rows; 
@@ -24,6 +26,10 @@ class Location
 
 class Participant
 {
+	const int participantID;
+	string name;
+	int age;
+	bool over18;
 
 };
 
@@ -31,7 +37,10 @@ enum TicketType { VIP, FrontSeat, MiddleSeat, UpperSeat };
 
 class Ticket
 {
-	
+	const int TicketID ;
+	bool boughtOnline; // is it bought online or not
+	int nr_of_agents; // number of agents that worked to make the ticket
+	float price;
 };
 
 

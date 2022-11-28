f4c14e40cd483010c4f7532eba3a11de43608306
Ion Cosulschi <cosulschiion20@stud.ase.ro>
2022-11-28 13:50:38 +0200

    Added 4 classes with some atributes and enumerations

--- TicketApp/TicketApp/Source.cpp
+++ TicketApp/TicketApp/Source.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
+
+enum EventType {Movie, Concert, Football, Workshop, Theatre, Other };
+class Event
+{
+	
+	string location;
+	char* eventName;
+	int nr_of_tickets_available; 
+	float* ticket_price;
+	EventType type;
+	bool IsOnline;
+
+};
+class Location
+{
+	string city; // The city the event is located
+	int* nr_seats_available; // depend on the number of rows the location has
+	int nr_rows; 
+
+};
+
+class Participant
+{
+
+};
+
+enum TicketType { VIP, FrontSeat, MiddleSeat, UpperSeat };
+
+class Ticket
+{
+	
+};
+
+
 
 int main ()
 {
-	cout << "Hello World";
-	cout << "Hi";
 	return 0;
 }
